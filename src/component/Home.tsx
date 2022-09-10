c93710251dc9d75dc7bb5ccfeb2615d4d0dd3a59
Ananya Teklu <teklu009@morris.umn.edu>
2022-09-10 16:57:24 -0500

    Implement the skeleton of the site - Home page added - Contact page added - Header and Footer components added - Routing for contact page is set - Logos added

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+const Salon = require ("../assets/Salon.jpg");
 
 const Home = () => {
   return (
-    <div>Queen Habesha Hair Brading</div>
+    <div>
+       <img className="Salon" src={Salon} alt="Salon" />
+    </div>
   )
 }
 
