7c669f65f897e7bd28c3daf19358874e2d2af398
Ananya Teklu <teklu009@morris.umn.edu>
2022-09-10 23:52:12 -0500

    Refine Styling for the image slider - Add boarder radius for slider images - Make image bigger upon hover - Bounce animation when the slider end is reached

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -18,10 +18,10 @@ useEffect(() => {
     <div>
       <img className="Salon" src={Salon} alt="Salon" />
       <motion.div ref={carousel} className='carousel' whileTap={{cursor:'grabbing'}}>
-        <motion.div drag = 'x' dragConstraints={{right: 0, left: - width}} className='inner-carousel'>
+        <motion.div drag = 'x' dragConstraints={{right: 0, left: - width}} dragTransition={{ bounceStiffness: 500, bounceDamping: 10 }} className='inner-carousel'>
           {styleImages.map((styleImage, index) => {
             return(
-              <motion.div className='item' key={styleImage}>
+              <motion.div className='item' key={styleImage} whileHover={{ scale: 1.2 }}>
                 <img src={styleImage} alt='Hair Styles'/>
               </motion.div>
             )
