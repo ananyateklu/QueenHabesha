f7af033fa313d7429147c615f8bb9b5988933c45
Ananya Teklu <teklu009@morris.umn.edu>
2022-09-17 17:07:30 -0500

    More Tweaking the booking page template

--- src/component/BookingPage.tsx
+++ src/component/BookingPage.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+const Helen = require("../assets/Helen.png");
 const BookingPage = () => {
-  return (
-    <div>BookingPage</div>
-  )
+    return (
+        <div className='BookingPage'>
+            <div className='HairStylistDiv'>
+            <img src={Helen} alt="Hair Styles" />
+            <h3>MAKE APPOINTMENT</h3>
+            <h4>HELEN HAILU </h4>
+            <h6>Please choose a time that works best for you. I look forward to working with you!</h6>
+            </div>
+            <div className='AppointmentDiv'>
+                <input className='TestInput'></input>
+                <button className='SignIn'>Sign In</button>
+            </div>
+        </div>
+
+    )
 }
 
 export default BookingPage
