aa04e5a83b6c7510faf8df10c2df5c3205a8adec
Fiseha Abebe <famarkfish79@gmail.com>
2022-09-20 08:28:26 +0300

    Testimonial Section Addition

--- Client/src/component/home.jsx
+++ Client/src/component/home.jsx
@@ -90,6 +90,73 @@ const Home = () => {
           <li className='BookingButton'>CONTACT US</li>
         </ul>
       </div>
+      
+      <div className="container">
+        
+      <div className="row">
+            <div className="col-lg-4">
+                <div className="card">
+                    <div className="face front-face">
+                    <img src={Mekdes} alt="Straight" className="profile"></img>
+                        <div className="pt-3 text-uppercase name">
+                            Hamelemal
+                        </div>
+                        <div className="designation">Customer One</div>
+                    </div>
+                    <div className="face back-face">
+                        <span className="fas fa-quote-left"></span>
+                        <div className="testimonial">
+                            I made bacck the purchase price in just 48 hours! Thank you for making it pain less,
+                            pleasant.
+                            The service was execellent. I will refer everyone I know.
+                        </div>
+                        <span className="fas fa-quote-right"></span>
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <div className="card">
+                    <div className="face front-face">
+                    <img src={Mekdes} alt="Straight" className="profile"></img>
+                        <div className="pt-3 text-uppercase name">
+                            Almaz
+                        </div>
+                        <div className="designation">Customer Two</div>
+                    </div>
+                    <div className="face back-face">
+                        <span className="fas fa-quote-left"></span>
+                        <div className="testimonial">
+                            Really good, you have saved our business! I made bacck the purchase price in just 48 hours!
+                            man, this thing is getting better and better as I learn more about it.
+                        </div>
+                        <span className="fas fa-quote-right"></span>
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <div className="card">
+                    <div className="face front-face">
+                    <img src={Mekdes} alt="Straight" className="profile"></img>
+                        <div className="pt-3 text-uppercase name">
+                            Hirut
+                        </div>
+                        <div className="designation">Customer Three</div>
+                    </div>
+                    <div className="face back-face">
+                        <span className="fas fa-quote-left"></span>
+                        <div className="testimonial">
+                            Account keeper is the most valuable business research we have EVER purchased. Without
+                            electrician, we would ahave gone bankrupt by now.
+                        </div>
+                        <span className="fas fa-quote-right"></span>
+                    </div>
+                </div>
+            </div>
+        </div>   
+                
+        
+   
+      </div>
     </div>
   );
 };
