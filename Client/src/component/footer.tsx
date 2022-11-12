275bc64ce38a612286185e4ef69ded3bdb0236d9
Fiseha Abebe <32363513+fiseha79@users.noreply.github.com>
2022-11-12 06:06:12 +0300

    changes to footer

--- Client/src/component/footer.tsx
+++ Client/src/component/footer.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import { HashLink } from 'react-router-hash-link'
-const FooterLogo = require("../assets/FooterLogo.png")
 
-
+const brandname_inv = require("../assets/BRANDNAME-INV.png")
+const brandsmall_inv = require("../assets/BRANDSMALL-INV.png")
+const logo_inv = require("../assets/logo-inverted.png")
 const footer = () => {
   return (
     <div className='FooterDiv'>
       <div className='FooterLeft'>
-        <img className="FooterLogo" src={FooterLogo} alt="logo" />
+      <div className="LogoDiv">
+        <div className="logo-img">
+          <img src={logo_inv} alt="logo"/>
+        </div>
+        <div className="BrandName">
+            <img src={brandname_inv} alt="brand name"/>
+        </div>
+        <div className="BrandName-small">
+        <img src={brandsmall_inv} alt="brandsmall"/>
+        </div>
+      </div>
         <h5>PHONE    +1(301) 433-1934</h5>
         <h5>EMAIL  Queenhabesha@gmail.com</h5>
         <h5>LOCATION   2917 Cliff Rd East Burnsville MN 55337 </h5>
