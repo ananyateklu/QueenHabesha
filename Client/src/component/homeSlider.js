150cb175fe1ba1c47ea25c88efe0bf9e536ec733
Fiseha Abebe <32363513+fiseha79@users.noreply.github.com>
2022-11-12 06:25:12 +0300

    changes to homeslider

--- Client/src/component/homeSlider.js
+++ Client/src/component/homeSlider.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-const img1 = require("../assets/QueenFront.png");
-
+const img1 = require("../assets/QueenFront2.jpg");
+const img2= require("../assets/Salon.jpg")
 
 export default class SimpleSlider extends Component {
     render() {
@@ -27,12 +27,12 @@ export default class SimpleSlider extends Component {
        
           
           <Slider {...settings}>
-          <div class="single-img kenburns-top">
+          <div class="single-img">
                 <img src={img1} alt=""/>
                 <h2>ONE</h2>
             </div>
-            <div class="single-img kenburns-top">
-            <img src={img1} alt=""/>
+            <div class="single-img ">
+            <img src={img2} alt=""/>
                 <h2>TWO</h2>
             </div>
             <div class="single-img">
